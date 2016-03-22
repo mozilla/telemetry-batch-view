e659d42228713ff91a7d12088fd82615ff04863a
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-03-22 17:35:54 +0000

    Improve HLL merging performance.

--- src/main/scala/spark/sql/aggregates.scala
+++ src/main/scala/spark/sql/aggregates.scala
@@ -1,40 +1,49 @@
 package telemetry.spark.sql.aggregates
 
-import com.twitter.algebird.{Bytes, DenseHLL, HyperLogLogMonoid}
+import com.twitter.algebird.{Bytes, DenseHLL, HyperLogLog}
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.expressions.{UserDefinedAggregateFunction, MutableAggregationBuffer}
 import org.apache.spark.sql.types._
 
-class HyperLogLog(bitSize: Int) extends UserDefinedAggregateFunction {
-  val monoid = new HyperLogLogMonoid(bits = bitSize)
-
+class HyperLogLogMerge extends UserDefinedAggregateFunction {
   def inputSchema: org.apache.spark.sql.types.StructType =
-    StructType(StructField("value", StringType) :: Nil)
+    StructType(StructField("value", BinaryType) :: Nil)
 
-  def bufferSchema: StructType = StructType(StructField("count", BinaryType) :: Nil)
+  def bufferSchema: StructType = StructType(StructField("count", BinaryType) ::
+                                            StructField("bits", IntegerType) :: Nil)
 
   def dataType: DataType = BinaryType
 
   def deterministic: Boolean = true
 
   def initialize(buffer: MutableAggregationBuffer): Unit = {
-    buffer(0) = monoid.zero.toDenseHLL.v.array
+    buffer(0) = null
+    buffer(1) = 0
   }
 
   def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
-    val state = new DenseHLL(bitSize, new Bytes(buffer.getAs[Array[Byte]](0)))
-    val x = monoid.toHLL(input.getAs[String](0))
-    buffer(0) = monoid.plus(state, x).toDenseHLL.v.array
+    val hll = HyperLogLog.fromBytes(input.getAs[Array[Byte]](0)).toDenseHLL
+
+    if (buffer(0) != null) {
+      hll.updateInto(buffer.getAs[Array[Byte]](0))
+    } else {
+      buffer(0) = hll.v.array
+      buffer(1) = hll.bits
+    }
   }
 
   def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
-    val state1 = new DenseHLL(bitSize, new Bytes(buffer1.getAs[Array[Byte]](0)))
-    val state2 = new DenseHLL(bitSize, new Bytes(buffer2.getAs[Array[Byte]](0)))
-    buffer1(0) = monoid.plus(state1, state2).toDenseHLL.v.array
+    if (buffer1(0) == null) {
+      buffer1(0) = buffer2(0)
+      buffer1(1) = buffer2(1)
+    } else if (buffer1(0) != null && buffer2(0) != null) {
+      val state2 = new DenseHLL(buffer2.getAs[Int](1), new Bytes(buffer2.getAs[Array[Byte]](0)))
+      state2.updateInto(buffer1.getAs[Array[Byte]](0))
+    }
   }
 
   def evaluate(buffer: Row): Any = {
-    val state = new DenseHLL(bitSize, new Bytes(buffer.getAs[Array[Byte]](0)))
+    val state = new DenseHLL(buffer.getAs[Int](1), new Bytes(buffer.getAs[Array[Byte]](0)))
     com.twitter.algebird.HyperLogLog.toBytes(state)
   }
 }
