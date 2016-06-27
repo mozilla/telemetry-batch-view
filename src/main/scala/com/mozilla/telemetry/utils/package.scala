c663affa8f629bfb69c42acd25195cfbf8d5236d
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-06-27 11:20:53 +0100

    Add timing utility function.

--- src/main/scala/com/mozilla/telemetry/utils/package.scala
+++ src/main/scala/com/mozilla/telemetry/utils/package.scala
@@ -101,4 +101,12 @@ package object utils{
     logger.info(s"Uploading Parquet file to $bucket/$key")
     s3.putObject(bucket, key, file)
   }
+
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block  // call-by-name
+    val t1 = System.nanoTime()
+    println(s"Elapsed time: ${(t1 - t0)/1000000000.0} s")
+    result
+  }
 }
