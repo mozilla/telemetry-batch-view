c42a80cb28f8da044aa2aaa08a9c69a5f7f08ca8
Ryan Harter <harterrt@gmail.com>
2016-09-02 10:09:31 -0700

    Refactor agg functions to a separate object
    Moved XSec generator functions into class methods and constructors.
    Separated Aggregate object into it's own context. Split aggregation into
    it's own package

---
+++ src/main/scala/com/mozilla/telemetry/utils/Aggregation.scala
@@ -0,0 +1,16 @@
+package com.mozilla.telemetry.utils
+
+package object aggregation {
+  def weightedMode[A](values: Seq[A], weights: Seq[Long]): A = {
+    if (values.size != weights.size) {
+      throw new IllegalArgumentException("Args to weighted mode must have the same length.")
+    } else if (values.size == 0) {
+      throw new IllegalArgumentException("Args to weighted mode must have length > 0.")
+    } else {
+      val pairs = values zip weights
+      val agg = pairs.groupBy(_._1).map(kv => (kv._1, kv._2.map(_._2).sum))
+      agg.maxBy(_._2)._1
+    }
+  }
+}
+
