b6699815c2b5f101aa047b8eeae872642de5fb5b
Ryan Harter <harterrt@gmail.com>
2016-09-20 17:12:32 -0700

    Add addon based fields

--- src/main/scala/com/mozilla/telemetry/utils/Aggregation.scala
+++ src/main/scala/com/mozilla/telemetry/utils/Aggregation.scala
@@ -1,15 +1,25 @@
 package com.mozilla.telemetry.utils
 
 package object aggregation {
-  def weightedMode[A](values: Seq[A], weights: Seq[Long]): A = {
-    if (values.size != weights.size) {
-      throw new IllegalArgumentException("Args to weighted mode must have the same length.")
-    } else if (values.size == 0) {
-      throw new IllegalArgumentException("Args to weighted mode must have length > 0.")
+  def weightedMean(values: Seq[Option[Long]], weights: Seq[Long]): Option[Double] = {
+    val clean_pairs = (values zip weights).map(pair => pair._1.map((_, pair._2))).flatten
+    val ttl_weight = clean_pairs.foldLeft(0l)((acc, pair) => acc + pair._2)
+    val sum_prod = clean_pairs.foldLeft(0.0)((acc, pair) => acc + (pair._1 * pair._2))
+
+    if ((ttl_weight <= 0) || (values.size != weights.size)) {
+      None
+    } else {
+      Some(sum_prod / ttl_weight)
+    }
+  }
+
+  def weightedMode[A](values: Seq[A], weights: Seq[Long]): Option[A] = {
+    if ((values.size != weights.size) || (values.size == 0)) {
+      None
     } else {
       val pairs = values zip weights
       val agg = pairs.groupBy(_._1).map(kv => (kv._1, kv._2.map(_._2).sum))
-      agg.maxBy(_._2)._1
+      Some(agg.maxBy(_._2)._1)
     }
   }
 }
--- src/main/scala/com/mozilla/telemetry/views/CrossSectionalView.scala
+++ src/main/scala/com/mozilla/telemetry/views/CrossSectionalView.scala
@@ -7,23 +7,48 @@ import org.apache.spark.sql.SQLContext
 import com.mozilla.telemetry.utils.S3Store
 import com.mozilla.telemetry.utils.aggregation
 
+case class ActiveAddon (
+  val blocklisted: Option[Boolean],
+  val description: Option[String],
+  val name: Option[String],
+  val user_disabled: Option[Boolean],
+  val app_disabled: Option[Boolean],
+  val version: Option[String],
+  val scope: Option[Long],
+  val `type`: Option[String],
+  val foreign_install: Option[Boolean],
+  val has_binary_components: Option[Boolean],
+  val install_day: Option[Long],
+  val update_day: Option[Long],
+  val signed_state: Option[Long],
+  val is_system: Option[Boolean]
+)
+
 object Longitudinal {
 }
 
 case class Longitudinal (
-    val client_id: String
-  , val normalized_channel: String
-  , val submission_date: Option[Seq[String]]
-  , val geo_country: Option[Seq[String]]
-  , val session_length: Option[Seq[Long]]
-  , val is_default_browser: Option[Seq[Option[Boolean]]]
-  , val default_search_engine: Option[Seq[Option[String]]]
-  , val locale: Option[Seq[Option[String]]]
-  , val architecture: Option[Seq[Option[String]]]
+  val client_id: String,
+  val normalized_channel: String,
+  val submission_date: Option[Seq[String]],
+  val geo_country: Option[Seq[String]],
+  val session_length: Option[Seq[Long]],
+  val is_default_browser: Option[Seq[Option[Boolean]]],
+  val default_search_engine: Option[Seq[Option[String]]],
+  val locale: Option[Seq[Option[String]]],
+  val architecture: Option[Seq[Option[String]]],
+  val active_addons: Option[Seq[Map[String, ActiveAddon]]]
 ) {
+  def weightedMean(values: Option[Seq[Option[Long]]]): Option[Double] = {
+    (values, this.session_length) match {
+      case (Some(v), Some(sl)) => aggregation.weightedMean(v, sl)
+      case _ => None
+    }
+  }
+
   def weightedMode[A](values: Option[Seq[A]]): Option[A] = {
     (values, this.session_length) match {
-      case (Some(v), Some(sl)) => Some(aggregation.weightedMode(v, sl))
+      case (Some(v), Some(sl)) => aggregation.weightedMode(v, sl)
       case _ => None
     }
   }
@@ -33,6 +58,22 @@ case class Longitudinal (
     this.submission_date.getOrElse(Seq()).map(date_parser.parse(_))
   }
 
+  def addonCount(): Option[Seq[Int]] = {
+    this.active_addons.map(_.map(_.size))
+  }
+
+  def foreignAddons(): Option[Seq[Map[String, ActiveAddon]]] = {
+    this.active_addons.map( // If array exists
+      _.map( // for each ping
+        _.filter(_._2.foreign_install.getOrElse(false))
+      )
+    )
+  }
+
+  def foreignAddonCount(): Option[Seq[Int]] = {
+    this.foreignAddons.map(_.map(_.size))
+  }
+
   def activeHoursByDOW(dow: Int) = {
     (this.session_length.getOrElse(Seq()) zip this.parsedSubmissionDate)
       .filter(_._2.getDay == dow).map(_._1).sum/3600.0
@@ -40,37 +81,49 @@ case class Longitudinal (
 }
 
 case class CrossSectional (
-    val client_id: String
-  , val normalized_channel: String
-  , val active_hours_total: Double
-  , val active_hours_0_mon: Double
-  , val active_hours_1_tue: Double
-  , val active_hours_2_wed: Double
-  , val active_hours_3_thu: Double
-  , val active_hours_4_fri: Double
-  , val active_hours_5_sat: Double
-  , val active_hours_6_sun: Double
-  , val geo_mode: Option[String]
-  , val geo_cfgs: Long
-  , val architecture_mode: Option[String]
-  , val ffLocale_mode: Option[String]
+  val client_id: String,
+  val normalized_channel: String,
+  val active_hours_total: Double,
+  val active_hours_0_mon: Double,
+  val active_hours_1_tue: Double,
+  val active_hours_2_wed: Double,
+  val active_hours_3_thu: Double,
+  val active_hours_4_fri: Double,
+  val active_hours_5_sat: Double,
+  val active_hours_6_sun: Double,
+  val geo_mode: Option[String],
+  val geo_cfgs: Long,
+  val architecture_mode: Option[String],
+  val ffLocale_mode: Option[String],
+  val addon_count_foreign_avg: Option[Double],
+  val addon_count_foreign_cfgs: Option[Long],
+  val addon_count_foreign_mode: Option[Long],
+  val addon_count_avg: Option[Double],
+  val addon_count_cfgs: Option[Long],
+  val addon_count_mode: Option[Long]
 ) {
   def this(base: Longitudinal) = {
     this(
-        client_id = base.client_id
-      , normalized_channel = base.normalized_channel
-      , active_hours_total = base.session_length.getOrElse(Seq()).sum
-      , active_hours_0_mon = base.activeHoursByDOW(1)
-      , active_hours_1_tue = base.activeHoursByDOW(2)
-      , active_hours_2_wed = base.activeHoursByDOW(3)
-      , active_hours_3_thu = base.activeHoursByDOW(4)
-      , active_hours_4_fri = base.activeHoursByDOW(5)
-      , active_hours_5_sat = base.activeHoursByDOW(6)
-      , active_hours_6_sun = base.activeHoursByDOW(0)
-      , geo_mode = base.weightedMode(base.geo_country)
-      , geo_cfgs = base.geo_country.getOrElse(Seq()).distinct.length
-      , architecture_mode = base.weightedMode(base.architecture).getOrElse(None)
-      , ffLocale_mode = base.weightedMode(base.locale).getOrElse(None)
+      client_id = base.client_id,
+      normalized_channel = base.normalized_channel,
+      active_hours_total = base.session_length.getOrElse(Seq()).sum / 3600.0,
+      active_hours_0_mon = base.activeHoursByDOW(1),
+      active_hours_1_tue = base.activeHoursByDOW(2),
+      active_hours_2_wed = base.activeHoursByDOW(3),
+      active_hours_3_thu = base.activeHoursByDOW(4),
+      active_hours_4_fri = base.activeHoursByDOW(5),
+      active_hours_5_sat = base.activeHoursByDOW(6),
+      active_hours_6_sun = base.activeHoursByDOW(0),
+      geo_mode = base.weightedMode(base.geo_country),
+      geo_cfgs = base.geo_country.getOrElse(Seq()).distinct.length,
+      architecture_mode = base.weightedMode(base.architecture).getOrElse(None),
+      ffLocale_mode = base.weightedMode(base.locale).getOrElse(None),
+      addon_count_foreign_avg = base.weightedMean(base.foreignAddonCount.map(_.map(x => Some(x.toLong)))),
+      addon_count_foreign_cfgs = base.foreignAddons.map(_.distinct.length),
+      addon_count_foreign_mode = base.weightedMode(base.foreignAddonCount).map(_.toLong),
+      addon_count_avg = base.weightedMean(base.addonCount.map(_.map(x => Some(x.toLong)))),
+      addon_count_cfgs = base.active_addons.map(_.distinct.length),
+      addon_count_mode = base.weightedMode(base.addonCount).map(_.toLong)
     )
   }
 }
@@ -90,6 +143,11 @@ object CrossSectionalView {
       "outName",
       descr = "Name for the output of this run",
       required = true)
+    val dryRun = opt[Boolean](
+      "dryRun",
+      descr = "Calculate the dataset, but do not write to S3",
+      required = false,
+      default=Some(false))
     verify()
   }
 
@@ -118,23 +176,25 @@ object CrossSectionalView {
       .selectExpr(
         "client_id", "normalized_channel", "submission_date", "geo_country",
         "session_length", "settings.locale", "settings.is_default_browser",
-        "settings.default_search_engine", "build.architecture"
+        "settings.default_search_engine", "build.architecture", "active_addons"
       )
       .as[Longitudinal]
     val output = ds.map(new CrossSectional(_))
 
     // Save to S3
-    val prefix = s"cross_sectional/${opts.outName()}"
-    val outputBucket = opts.outputBucket()
-    val path = s"s3://${outputBucket}/${prefix}"
+    if (!opts.dryRun()) {
+      val prefix = s"cross_sectional/${opts.outName()}"
+      val outputBucket = opts.outputBucket()
+      val path = s"s3://${outputBucket}/${prefix}"
 
 
-    require(S3Store.isPrefixEmpty(outputBucket, prefix),
-      s"${path} already exists!")
+      require(S3Store.isPrefixEmpty(outputBucket, prefix),
+        s"${path} already exists!")
 
-    output.toDF().write.parquet(path)
+      output.toDF().write.parquet(path)
+    }
 
-    val ex = output.take(2)
+    val ex = output.cache.take(2)
     println("="*80 + "\n" + ex + "\n" + "="*80)
 
     sc.stop()
