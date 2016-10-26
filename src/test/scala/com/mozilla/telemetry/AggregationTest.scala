d2975bcaee13b03286605315b3925c73b7a4c839
Ryan Harter <harterrt@gmail.com>
2016-10-26 15:22:13 -0400

    Filter bad subsession lengths

--- src/test/scala/com/mozilla/telemetry/AggregationTest.scala
+++ src/test/scala/com/mozilla/telemetry/AggregationTest.scala
@@ -8,28 +8,24 @@ class AggregationTest extends FlatSpec {
   private val tol= 1e-7
 
   "The weighted mode" must "combine repeated keys" in {
-    val mode = aggregation.weightedMode(Seq("DE", "IT", "DE"), Seq(3, 6, 4))
+    val mode = aggregation.weightedMode(Seq(("DE", 3l), ("IT", 6l), ("DE", 4l)))
     assert(mode == Some("DE"))
   }
 
   it must "respect weights" in {
-    val mode = aggregation.weightedMode(Seq("DE", "IT", "IT"), Seq(3, 1, 1))
+    val mode = aggregation.weightedMode(Seq(("DE", 3l), ("IT", 1l), ("IT", 1l)))
     assert(mode == Some("DE"))
   }
 
   "The weighted mean" must "handle empty sequences" in {
-    assert(aggregation.weightedMean(Seq(), Seq()) == None)
+    assert(aggregation.weightedMean(Seq()) == None)
   }
   
   it must "handle 0 weights" in {
-    assert(aggregation.weightedMean(Seq(Some(1)), Seq(0)) == None)
+    assert(aggregation.weightedMean(Seq((1, 0))) == None)
   }
 
   it must "respect weights" in {
-    aggregation.weightedMean(Seq(Some(1), Some(2)), Seq(1, 2)).getOrElse(0.0) shouldBe ((5.0 / 3) +- tol)
-  }
-
-  it must "exclude missing values" in {
-    aggregation.weightedMean(Seq(Some(1), None), Seq(1, 2)).getOrElse(0.0) shouldBe (1.0 +- tol)
+    aggregation.weightedMean(Seq((1, 1), (2, 2))).getOrElse(0.0) shouldBe ((5.0 / 3) +- tol)
   }
 }
