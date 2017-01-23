ff6e75f5b24ce88d0237b5c890ea8692baef5fcf
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2017-01-23 16:47:40 +0000

    Generate protobuf classes at compile time.
    The externally generated protobuf class wasn't compatible with Flink (it was
    throwing some serialization errors). This patch fixes that and also
    ensures that a change in the protobuf file doesn't require an external
    recompiliation step.

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,6 @@
 resolvers += "bintray-spark-packages" at "https://dl.bintray.com/spark-packages/maven/"
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.1.0")
+addSbtPlugin("com.thesamet" % "sbt-protoc" % "0.99.3")
+
+libraryDependencies += "com.trueaccord.scalapb" %% "compilerplugin" % "0.5.47"
--- src/main/scala/com/mozilla/telemetry/heka/package.scala
+++ src/main/scala/com/mozilla/telemetry/heka/package.scala
@@ -2,7 +2,7 @@ package com.mozilla.telemetry
 
 package object heka {
   class RichMessage(m: Message) {
-    def fieldsAsMap(): Map[String, Any] = {
+    def fieldsAsMap: Map[String, Any] = {
       val fields = m.fields
       Map(fields.map(_.name).zip(fields.map(field)): _*)
     }
@@ -10,16 +10,16 @@ package object heka {
 
   private def field(f: Field): Any = {
     // I am assuming there is only one value
-    f.valueType match {
-      case Some(Field.ValueType.BYTES) => {
+    f.getValueType match {
+      case Field.ValueType.BYTES => {
         val bytes = f.valueBytes(0)
         // Non-UTF8 bytes fields are currently not supported
         if (bytes.isValidUtf8) bytes.toStringUtf8 else ""
       }
-      case Some(Field.ValueType.STRING) => f.valueString(0)
-      case Some(Field.ValueType.BOOL) => f.valueBool(0)
-      case Some(Field.ValueType.DOUBLE) => f.valueDouble(0)
-      case Some(Field.ValueType.INTEGER) => f.valueInteger(0)
+      case Field.ValueType.STRING => f.valueString(0)
+      case Field.ValueType.BOOL => f.valueBool(0)
+      case Field.ValueType.DOUBLE => f.valueDouble(0)
+      case Field.ValueType.INTEGER => f.valueInteger(0)
       case _ => ""
     }
   }
