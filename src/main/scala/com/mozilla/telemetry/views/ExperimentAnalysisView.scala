04e40eab660e6cac97dc8415475b333f8f13ccd4
Frank Bertsch <fbertsch@mozilla.com>
2017-06-30 14:32:25 -0700

    Add aggregates of derived values to experiments

--- src/main/scala/com/mozilla/telemetry/views/ExperimentAnalysisView.scala
+++ src/main/scala/com/mozilla/telemetry/views/ExperimentAnalysisView.scala
@@ -2,7 +2,8 @@ package com.mozilla.telemetry.views
 
 import com.mozilla.telemetry.experiments.analyzers.{HistogramAnalyzer, ScalarAnalyzer}
 import com.mozilla.telemetry.metrics._
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types._
 import org.rogach.scallop.ScallopConf
 import org.apache.spark.sql.functions.col
 
@@ -10,8 +11,30 @@ object ExperimentAnalysisView {
   def schemaVersion: String = "v1"
   def jobName: String = "experiment_analysis"
 
+  // All non-scalars and non-numeric scalars are ignored
+  def Blacklist = 
+    "client_id" ::
+    "document_id" ::
+    "experiment_branch" ::
+    "experiment_id" ::
+    "install_year" ::
+    "is_wow64" ::
+    "memory_mb" ::
+    "normalized_channel" ::
+    "os_service_pack_major" ::
+    "os_service_pack_minor" ::
+    "profile_creation_date" ::
+    "profile_subsession_counter" ::
+    "sample_id" ::
+    "submission_date_s3" ::
+    "subsession_counter" ::
+    "timestamp" ::
+    "timezone_offset" ::
+    "windows_build_number" ::
+    "windows_ubr" :: Nil
+
   // Configuration for command line arguments
-  private class Conf(args: Array[String]) extends ScallopConf(args) {
+  class Conf(args: Array[String]) extends ScallopConf(args) {
     // TODO: change to s3 bucket/keys
     val inputLocation = opt[String]("input", descr = "Source for parquet data", required = true)
     val outputLocation = opt[String]("output", descr = "Destination for parquet data", required = true)
@@ -22,13 +45,7 @@ object ExperimentAnalysisView {
   }
 
   def main(args: Array[String]) {
-    // Spark/job setup stuff
     val conf = new Conf(args)
-    val date = conf.date.get match {
-      case Some(d) => d
-      case _ => com.mozilla.telemetry.utils.yesterdayAsYYYYMMDD
-    }
-
     val spark = SparkSession
       .builder()
       .master("local[*]")
@@ -41,45 +58,88 @@ object ExperimentAnalysisView {
     hadoopConf.set("parquet.enable.summary-metadata", "false")
 
     val data = spark.read.parquet(conf.inputLocation())
+    val date = getDate(conf)
 
-    val experiments = conf.experiment.get match {
-      case Some(e) => List(e)
-      // if there isn't a specific experiment passed in, run the job for all experiments that had pings on the date
-      // we're running on
-      case _ => data
-        .where(col("submission_date_s3") === date)
-        .select("experiment_id")
-        .distinct()
-        .collect()
-        .toList
-        .map(r => r(0).asInstanceOf[String])
-    }
-
-    val metricList: List[(String, MetricDefinition)] = conf.metric.get match {
-      case Some(m) => List((m, (Histograms.definitions() ++ Scalars.definitions())(m.toUpperCase)))
-      case _ => MainSummaryView.filterHistogramDefinitions(Histograms.definitions(), useWhitelist = true) ++
-        Scalars.definitions(includeOptin = true).toList
-    }
-
-    experiments.foreach { e: String =>
-      val experimentData = data.where(col("experiment_id") === e)
-      val experimentResult = metricList.map {
-        case (name: String, hd: HistogramDefinition) =>
-          val columnName = MainSummaryView.getHistogramName(name.toLowerCase, "parent")
-          new HistogramAnalyzer(columnName, hd, experimentData).analyze()
-        case (name: String, sd: ScalarDefinition) =>
-          val columnName = Scalars.getParquetFriendlyScalarName(name, "parent")
-          ScalarAnalyzer.getAnalyzer(columnName, sd, experimentData).analyze()
-        case _ => throw new UnsupportedOperationException("Unsupported metric definition type")
-      }.reduce(_.union(_))
-
+    getExperiments(conf, data).foreach{ e: String => 
       val outputLocation = s"${conf.outputLocation()}/experiment_id=$e/date=$date"
-      experimentResult.toDF
+      getExperimentMetrics(e, data, conf)
         .drop(col("experiment_id"))
         .repartition(1)
         .write.mode("overwrite").parquet(outputLocation)
     }
 
     spark.stop()
+  }
+
+  def getDate(conf: Conf): String =  {
+   conf.date.get match {
+      case Some(d) => d
+      case _ => com.mozilla.telemetry.utils.yesterdayAsYYYYMMDD
+    }   
+  }
+
+  def getExperiments(conf: Conf, data: DataFrame): List[String] = {
+    conf.experiment.get match {
+      case Some(e) => List(e)
+      case _ => data // get all experiments
+        .where(col("submission_date_s3") === getDate(conf))
+        .select("experiment_id")
+        .distinct()
+        .collect()
+        .toList
+        .map(r => r(0).asInstanceOf[String])
+    }
+  }
+
+  def getMetrics(conf: Conf, data: DataFrame) = {
+    conf.metric.get match {
+      case Some(m) => {
+        val histogramDefs = Histograms.definitions()
+        val scalarDefs = Scalars.definitions()
+        val otherDefs = getOtherDefinitions(data, scalarDefs.toList, histogramDefs.toList).toMap
+        List((m, (histogramDefs ++ scalarDefs ++ otherDefs)(m.toUpperCase)))
+      }
+      case _ => {
+        val histogramDefs = MainSummaryView.filterHistogramDefinitions(Histograms.definitions(), useWhitelist = true)
+        val scalarDefs = Scalars.definitions(includeOptin = true).toList
+        scalarDefs ++ histogramDefs ++ getOtherDefinitions(data, scalarDefs, histogramDefs)
+      }
+    }
+  }
+
+  def getExperimentMetrics(experiment: String, data: DataFrame, conf: Conf): DataFrame = {
+    val metricList = getMetrics(conf, data)
+    val experimentData = data.where(col("experiment_id") === experiment)
+
+    metricList.map {
+      case (name: String, hd: HistogramDefinition) =>
+        val columnName = MainSummaryView.getHistogramName(name.toLowerCase, "parent")
+        new HistogramAnalyzer(columnName, hd, experimentData).analyze()
+      case (name: String, sd: DerivedScalarDefinition) =>
+        ScalarAnalyzer.getAnalyzer(name, sd, experimentData).analyze()
+      case (name: String, sd: ScalarDefinition) =>
+        val columnName = Scalars.getParquetFriendlyScalarName(name, "parent")
+        ScalarAnalyzer.getAnalyzer(columnName, sd, experimentData).analyze()
+      case _ => throw new UnsupportedOperationException("Unsupported metric definition type")
+    }.reduce(_.union(_)).toDF()
+  }
+
+  def getOtherDefinitions(df: DataFrame, scalarDefs: List[(String, ScalarDefinition)], histogramDefs: List[(String, HistogramDefinition)]): List[(String, MetricDefinition)] = {
+    val excludedFields = 
+      scalarDefs.map(_._1).toSet ++ 
+      histogramDefs.map(_._1).toSet ++
+      Blacklist.toSet
+
+    df.schema.fields
+      .filter(sf => !excludedFields.contains(sf.name))
+      .map{ sf => sf.dataType match {
+          case _: IntegerType => Some((sf.name, new UintDerivedScalar))
+          case _: LongType => Some((sf.name, new LongDerivedScalar))
+          case _: BooleanType => Some((sf.name, new BooleanDerivedScalar))
+          case _ => None
+        }
+      }
+      .flatten
+      .toList
   }
 }
