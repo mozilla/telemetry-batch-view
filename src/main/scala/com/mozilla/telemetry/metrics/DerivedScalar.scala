3d7b1ddefb335d3d2a899fb7ef1ebcca8da1c021
Sunah Suh <github@sunahsuh.com>
2017-07-11 15:35:58 -0500

    Revert "Add aggregates of derived values to experiments"
    This reverts commit 04e40eab660e6cac97dc8415475b333f8f13ccd4.

--- src/main/scala/com/mozilla/telemetry/metrics/DerivedScalar.scala
+++
@@ -1,7 +0,0 @@
-package com.mozilla.telemetry.metrics
-
-sealed abstract class DerivedScalarDefinition extends ScalarDefinition
-case class UintDerivedScalar(keyed: Boolean = false, processes: List[String] = Nil) extends DerivedScalarDefinition
-case class LongDerivedScalar(keyed: Boolean = false, processes: List[String] = Nil) extends DerivedScalarDefinition
-case class BooleanDerivedScalar(keyed: Boolean = false, processes: List[String] = Nil) extends DerivedScalarDefinition
-case class StringDerivedScalar(keyed: Boolean = false, processes: List[String] = Nil) extends DerivedScalarDefinition
