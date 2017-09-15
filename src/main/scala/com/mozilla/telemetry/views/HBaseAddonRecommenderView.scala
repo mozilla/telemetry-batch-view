828765621875e5173801c1e543e9ba86526a36e6
Alessio Placitelli <alessio.placitelli@gmail.com>
2017-09-15 14:41:27 +0200

    Add the "disabled_addons_ids" field to the TAAR HBase
    This fields contains the list of addon ids that were
    disabled and are not reported in active_addons. See
    bug 1390814.

--- src/main/scala/com/mozilla/telemetry/views/HBaseAddonRecommenderView.scala
+++ src/main/scala/com/mozilla/telemetry/views/HBaseAddonRecommenderView.scala
@@ -87,7 +87,8 @@ object HBaseAddonRecommenderView {
             $"scalar_parent_browser_engagement_tab_open_event_count",
             $"scalar_parent_browser_engagement_total_uri_count",
             $"scalar_parent_browser_engagement_unique_domains_count",
-            $"active_addons")
+            $"active_addons",
+            $"disabled_addons_ids")
 
         // Join the two tables: only the elements in both dataframes will make it
         // through.
