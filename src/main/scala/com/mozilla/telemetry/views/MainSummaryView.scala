635f4cc9982da8bca6a049e8300a2fe47c4998eb
Frank Bertsch <fbertsch@mozilla.com>
2017-10-06 16:52:27 -0500

    Add e10sMultiProcess to main_summary

--- src/main/scala/com/mozilla/telemetry/views/MainSummaryView.scala
+++ src/main/scala/com/mozilla/telemetry/views/MainSummaryView.scala
@@ -568,6 +568,10 @@ object MainSummaryView {
           case JString(x) => x
           case _ => null
         },
+        settings \ "e10sMultiProcesses" match {
+          case JInt(x) => x.toLong
+          case _ => null
+        },
         settings \ "locale" match {
           case JString(x) => x
           case _ => null
@@ -939,6 +943,10 @@ object MainSummaryView {
       // See bug 1251259
       StructField("e10s_enabled", BooleanType, nullable = true), // environment/settings/e10sEnabled
       StructField("e10s_cohort", StringType, nullable = true), // environment/settings/e10sCohort
+
+      // Bug 1406238
+      StructField("e10s_multi_processes", LongType, nullable = true), // environment/settings/e10sMultiProcesses
+
       StructField("locale", StringType, nullable = true), // environment/settings/locale
       // See bug 1331082
       StructField("attribution", buildAttributionSchema, nullable = true), // environment/settings/attribution/
