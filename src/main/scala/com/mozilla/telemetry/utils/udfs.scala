141a9a00d7d7c91a52cd63b568b36ac9870f7d7b
Daniel Thorn <dthorn@mozilla.com>
2018-02-05 16:12:51 -0800

    Add bucketed() udf to utils and test in GenericCountView
    also set mergeSchema=true for GenericCountView

--- src/main/scala/com/mozilla/telemetry/utils/udfs.scala
+++ src/main/scala/com/mozilla/telemetry/utils/udfs.scala
@@ -169,10 +169,15 @@ object UDFs{
   val HllMerge = new HyperLogLogMerge
   val FilteredHllMerge = new FilteredHyperLogLogMerge
 
+  private def bucketed(value: Float, splits: Seq[Int]): Int = splits.collectFirst {
+    case e if value <= e => e
+  }.getOrElse(splits.max + 1)
+
   implicit class MozUDFs(spark: SparkSession) {
     def registerUDFs: Unit = {
       spark.udf.register(HllCreate, hllCreate _)
       spark.udf.register(HllCardinality, hllCardinality _)
+      spark.udf.register("bucketed", bucketed _)
     }
   }
 }
