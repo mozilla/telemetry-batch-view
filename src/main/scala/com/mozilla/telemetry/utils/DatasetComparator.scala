329671220c2c1ef5fa38c76d3c622c5e4867d1bf
Daniel Thorn <dthorn@mozilla.com>
2018-04-17 11:40:47 -0700

    Clean up DatasetComparator
    and make printing results optional

--- src/main/scala/com/mozilla/telemetry/utils/DatasetComparator.scala
+++ src/main/scala/com/mozilla/telemetry/utils/DatasetComparator.scala
@@ -6,6 +6,11 @@ import org.rogach.scallop._
 object DatasetComparator {
   def jobName: String = "dataset_comparator"
 
+  case class Columns(originalOnly: Seq[String], testOnly: Seq[String], both: Seq[String])
+  case class Count(original: Long, test: Long, change: Long)
+  case class Values(originalOnly: Long, testOnly: Long)
+  case class Result(columns: Columns, nulls: Map[String, Count], rows: Count, values: Values, same: Boolean = false)
+
   // Configuration for command line arguments
   class Conf(args: Array[String]) extends ScallopConf(args) {
     val date = opt[String]("date", descr = "Date to compare", required = true)
@@ -18,6 +23,7 @@ object DatasetComparator {
     val testBucket = opt[String]("test-bucket", descr = "Source bucket for test data", required = false, default = Some("telemetry-test-bucket"))
     val testViewname = opt[String]("test-viewname", descr = "View to pull test data from. Cannot be used with --test-bucket", required = false)
     val where = opt[String]("where", descr = "Filter statement for the data to compare; e.g. \"sample_id = '0'\"", required = false)
+    val printResult = opt[Boolean]("print-result", descr = "Whether or not to print job results", required = false, default = Some(true))
     val resultPath = opt[String]("result-path", descr = "S3 location to write results to in json format", required = false)
 
     conflicts(originalViewname, List(originalBucket))
@@ -29,11 +35,48 @@ object DatasetComparator {
     val conf = new Conf(args) // parse command line arguments
     // Set up Spark
     val spark = getOrCreateSparkSession(jobName)
-    val valid = validate(spark, conf)
+    val result = compare(spark, conf)
+    output(spark, conf, result)
     spark.stop()
-    if (!valid && conf.failWithoutMatch()) {
+    if (!result.same && conf.failWithoutMatch()) {
       System.exit(1)
     }
+  }
+
+  def output(spark: SparkSession, conf: Conf, result: Result): Unit = {
+    if (conf.resultPath.isDefined) {
+      import spark.sqlContext.implicits._
+      List(result).toDF.write.mode("overwrite").json(conf.resultPath())
+    }
+
+    if (conf.printResult()) {
+      println(s"Common columns: ${result.columns.both.mkString(",")}")
+      println(s"Added columns: ${result.columns.testOnly.mkString(",")}")
+      println(s"Dropped columns: ${result.columns.originalOnly.mkString(",")}")
+      println(s"Row count went from ${result.rows.original} to ${result.rows.test}")
+      println(s"Added ${result.values.testOnly} row values")
+      println(s"Dropped ${result.values.originalOnly} row values")
+      result.nulls.foreach { pair =>
+        val (col, count) = pair
+        println(s"Null values in '$col' went from ${count.original} to ${count.test}")
+      }
+      println(s"Datasets are${if (result.same) "" else " not"} the same")
+    }
+  }
+
+  def compare(spark: SparkSession, conf: Conf): Result = {
+    val original = getDataFrame(spark, conf.originalViewname, conf.originalBucket, conf.dataPath(), conf.dateField(), conf.date(), conf.where)
+    val test = getDataFrame(spark, conf.testViewname, conf.testBucket, conf.dataPath(), conf.dateField(), conf.date(), conf.where)
+
+    val result: Result = Result(
+      columns = checkColumns(original, test),
+      nulls = checkNulls(original, test),
+      rows = checkRows(original, test),
+      values = checkValues(original, test, conf.select())
+    )
+    val same = checkSame(result)
+
+    result.copy(same = same)
   }
 
   def getDataFrame(spark: SparkSession, view: ScallopOption[String], bucket: ScallopOption[String], dataPath: String, dateField: String, date: String, where: ScallopOption[String]): DataFrame = {
@@ -47,106 +90,65 @@ object DatasetComparator {
     }
   }
 
-  def selectColumns(select: String, df: DataFrame): DataFrame = df.select(select.split(",").map{c=>df.col(c)}:_*)
+  // intersection and difference of columns
+  def checkColumns(original: DataFrame, test: DataFrame): Columns = {
+    val originalColumns: Set[String] = original.columns.map{c=>s"$c"}.toSet
+    val testColumns: Set[String] = test.columns.map{c=>s"$c"}.toSet
 
-  def validate(spark: SparkSession, conf: Conf): Boolean = {
-    val dataPath = conf.dataPath()
-    val date = conf.date()
-    val dateField = conf.dateField()
-    val select = conf.select()
+    Columns(
+      originalColumns.diff(testColumns).toSeq,
+      testColumns.diff(originalColumns).toSeq,
+      (originalColumns & testColumns).toSeq
+    )
+  }
 
-    println("=======================================================================================")
-    println(s"BEGINNING JOB $jobName FOR $date")
+  // null count by column
+  def checkNulls(original: DataFrame, test: DataFrame): Map[String, Count] = {
+    val intersectCols = (original.columns.toSet & test.columns.toSet).toSeq
+    val query = intersectCols.map(col => s"sum(cast(($col is null) as int)) as $col")
+    val originalNulls = original.selectExpr(query:_*).first().toSeq
+    val testNulls = test.selectExpr(query:_*).first().toSeq
 
-    val original = getDataFrame(spark, conf.originalViewname, conf.originalBucket, dataPath, dateField, date, conf.where)
-    val test = getDataFrame(spark, conf.testViewname, conf.testBucket, dataPath, dateField, date, conf.where)
+    (intersectCols zip (originalNulls zip testNulls)).map{ m =>
+      val (col: String, (original: Long, test: Long)) = m
+      col -> Count(original, test, test - original)
+    }.toMap
+  }
 
-    var same = true
-
-    // compare row counts
+  // row count
+  def checkRows(original: DataFrame, test: DataFrame): Count = {
     val originalCount = original.count
     val testCount = test.count
-    val countChange = testCount - originalCount
-    if (originalCount != testCount) {
-      println(s"Wrong number of rows. Expected $originalCount. Got $testCount")
-      same = false
-    } else {
-      println(s"Same number of rows. Expected $originalCount. Got $testCount")
-    }
 
-    // compare null count in each column
-    val intersectCols = (original.columns.toSet & test.columns.toSet).toSeq
+    Count(
+      original = originalCount,
+      test = testCount,
+      change = testCount - originalCount
+    )
+  }
 
-    val query = intersectCols.map(col => s"sum(cast(($col is null) as int)) as $col")
-    val originalNulls = original.selectExpr(query:_*).first()
-    val testNulls = test.selectExpr(query:_*).first()
-
-    val nullsChange: scala.collection.mutable.Map[String, Long] = scala.collection.mutable.Map()
-    for ((col: String, (original: Long, test: Long)) <- intersectCols zip (originalNulls.toSeq zip testNulls.toSeq)) {
-      nullsChange += (col -> (test - original))
-      if (original != test)  {
-        println(s"Wrong number of null values in '$col'. Expected $original. Got $test")
-        same = false
-      } else {
-        println(s"Same number of null values in '$col'. Expected $original. Got $test")
-      }
-    }
-
-    val missingColumns = original.columns.flatMap { col =>
-      if (!(test.columns contains col)) {
-        println(s"Missing column '$col'.")
-        same = false
-        Some(col)
-      } else {
-        println(s"Found column '$col'.")
-        None
-      }
-    }
-
-    // check for extra columns
-    val extraColumns = test.columns.flatMap { col =>
-      if (!(original.columns contains col)) {
-        println(s"Extra column '$col'.")
-        same = false
-        Some(col)
-      } else {
-        println(s"Found column '$col'.")
-        None
-      }
-    }
-
-    // compare comparable columns
+  // distinct value count for `select` columns that DataFrame.except works on
+  def checkValues(original: DataFrame, test: DataFrame, select: String): Values = {
     val originalColumns = selectColumns(select, original)
     val testColumns = selectColumns(select, test)
 
-    val missing = originalColumns.except(testColumns).count
-    println(s"$missing missing value(s)")
-    if (missing > 0) { same = false }
-
-    val extra = testColumns.except(originalColumns).count
-    println(s"$extra extra value(s)")
-    if (extra > 0) { same = false }
-
-    if (conf.resultPath.isDefined) {
-      import spark.sqlContext.implicits._
-      List(Result(countChange, nullsChange, missingColumns, extraColumns, missing, extra, same)).toDF.write.mode("overwrite").json(conf.resultPath())
-    }
-
-    val status = if (same) "SUCCESS" else "FAILURE"
-
-    println(s"$status ON JOB $jobName FOR $date")
-    println("=======================================================================================")
-
-    same
+    Values(
+      originalOnly = originalColumns.except(testColumns).count,
+      testOnly = testColumns.except(originalColumns).count
+    )
   }
 
-  case class Result(
-    countChange: Long,
-    nullsChange: scala.collection.mutable.Map[String, Long],
-    missingColumns: Array[String],
-    extraColumns: Array[String],
-    missingValueCount: Long,
-    extraValueCount: Long,
-    same: Boolean
+  // select columns from `df` where `select` is a comma separated list of columns
+  def selectColumns(select: String, df: DataFrame): DataFrame = df.select(
+    select.split(",").map{c=>df.col(c)}:_*
   )
+
+  def checkSame(result: Result): Boolean = {
+    result.columns.originalOnly.isEmpty &&
+    result.columns.testOnly.isEmpty &&
+    result.nulls.forall(pair => pair._2.change == 0) &&
+    result.rows.change == 0 &&
+    result.values.originalOnly == 0 &&
+    result.values.testOnly == 0
+  }
 }
