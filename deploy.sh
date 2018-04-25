6ea48041b6ea3916da932c174a313ca0fde5a759
Frank Bertsch <fbertsch@mozilla.com>
2018-04-25 15:04:55 -0500

    WIP: Deploy pieces

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ -z "$JAR" ]]; then
+    echo "Missing arguments!" 1>&2
+    exit 1
+fi
+
+TRAVIS_REPO_OWNER=${TRAVIS_REPO_SLUG%/*}
+SLUG=${TRAVIS_REPO_SLUG#*/}
+JAR_DIR="$TRAVIS_REPO_OWNER/$SLUG"
+JAR_NAME="$SLUG.jar"
+TXT_NAME="$SLUG.txt"
+
+sbt assembly
+
+if [[ -z "$TRAVIS_TAG" ]]; then
+    BRANCH_OR_TAG=$TRAVIS_BRANCH
+    ID=$TRAVIS_JOB_NUMBER
+else
+    BRANCH_OR_TAG=tags
+    ID=$TRAVIS_TAG
+fi
+
+CANONICAL_JAR="$JAR_DIR/$BRANCH_OR_TAG/$ID/$JAR_NAME"
+echo $CANONICAL_JAR > $TXT_NAME
+
+mkdir -p $JAR_DIR/$BRANCH_OR_TAG/$ID
+cp $JAR $CANONICAL_JAR
+cp $JAR "$JAR_DIR/$BRANCH_OR_TAG/$JAR_NAME"
+cp $TXT_NAME "$JAR_DIR/$BRANCH_OR_TAG/$TXT_NAME"
+
+curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
+
+export ARTIFACTS_PERMISSIONS="public-read"
+export ARTIFACTS_PATHS="$TRAVIS_REPO_OWNER/"
+export ARTIFACTS_TARGET_PATHS="/$TRAVIS_REPO_OWNER/"
+artifacts upload
