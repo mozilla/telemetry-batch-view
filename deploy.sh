da6ca212e75264c115b944ab4ad2c03235fdd15c
Harold Woo <hwoo@mozilla.com>
2018-05-08 11:57:28 -0700

    [Bug 1457938] Modify travis build to only upload artifacts for master tags

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 if [[ -z "$JAR" ]]; then
     echo "Missing arguments!" 1>&2
@@ -20,6 +21,19 @@ if [[ -z "$TRAVIS_TAG" ]]; then
     BRANCH_OR_TAG=$TRAVIS_BRANCH
     ID=$TRAVIS_JOB_NUMBER
 else
+    # Only continue uploading jar if the tag was on master branch
+    pushd ${TRAVIS_BUILD_DIR}
+    # Finds all branches that contain the commit sha of TRAVIS_TAG
+    output=$(git branch -r --contains `git rev-parse --verify ${TRAVIS_TAG}^{commit}`)
+
+    if [[ $output =~ .*origin\/master.* ]]; then
+        echo "Tag ${TRAVIS_TAG} is on master branch. Continuing upload..."
+    else
+        echo "Tag ${TRAVIS_TAG} is not on master branch. Skipping upload"
+        # Exit 0 so the travis build doesn't fail
+        exit 0
+    fi
+    popd
     BRANCH_OR_TAG=tags
     ID=$TRAVIS_TAG
 fi
