7091a1168428c5398e69e73ca94ae2f47b3e9c9f
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-09-05 17:27:02 -0700

    Update the testing script for a 1 minute timeout

--- scripts/test_run.sh
+++ scripts/test_run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set -euo pipefail
+set -uo pipefail
 set -x
 
 tbv_jar=$1
-date=${2:-$(date +%Y%m%d --date=yesterday)}
+date=${2:-$(date +%Y%m%d --date='2 days ago')}
 bucket=${3:-"telemetry-test-bucket"}
+timeout=${4:-60}
 
 if [[ -z ${tbv_jar} ]]; then
     echo "Missing telemetry-batch-view jar!"
@@ -22,33 +23,39 @@ submit() {
     classname="$1"
     options="$2"
 
-    spark-submit \
+    timeout $timeout spark-submit \
         --master yarn \
         --deploy-mode client \
         --class com.mozilla.telemetry.views.${classname} \
         ${tbv_jar} ${options}
+
+    # A timeout results in an error code of 124
+    rv=$?
+    if [[ $rv != 0 && $rv != 124 ]]; then
+        echo "bad error code for $classname";
+        exit 1;
+    fi
 }
 
 baseopt="--to $date --from $date --bucket $bucket"
 
 submit MainSummaryView          "$baseopt --channel nightly --read-mode aligned"
-submit ExperimentSummaryView    "$baseopt --experiment-limit 1"
+submit SyncView                 "$baseopt"
+submit CrashAggregateView       "$baseopt"
+submit MainEventsView           "$baseopt --inbucket telemetry-parquet"
+submit AddonsView               "$baseopt --inbucket telemetry-parquet"
+submit CrashSummaryView         "--to $date --from $date --outputBucket $bucket --dryRun"
+submit LongitudinalView         "--to $date --bucket $bucket"
+submit ExperimentAnalysisView   "--date $date --input s3://telemetry-parquet/experiments/v1 --output s3://$bucket/experiments_aggregates/v1"
+submit ExperimentSummaryView    "$baseopt --inbucket telemetry-parquet --experiment-limit 1"
 
-# long running
-submit CrashSummaryView         "$baseopt --dry-run"
 
 ###########################################################
 #               Jobs without Test Options
 ###########################################################
 
-# submit AddonsView
-# submit CrashAggregateView
-# submit ExperimentAnalysisView
-# submit LongitudinalView
-# submit MainEventsView
 # submit SyncEventView
 # submit SyncFlatView
-# submit SyncView
 
 # submit GenericCountView
 # submit GenericLongitudinalView
